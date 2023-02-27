0f299eee72c71551578bc4896b88f82f97adb647
Ernesto De la Rosa <ernestodelazamora@gmail.com>
2023-02-27 17:30:42 -0600

    analitics added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,18 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 
 /**
  * Es una función que toma un Componente y pageProps y devuelve un Componente con pageProps
  * @param {AppProps}  - Componente: el componente de la página que se está representando.
  * @returns El componente que se está representando.
  */
-export default function App({ Component, pageProps }: AppProps) {
-	return <Component {...pageProps} />;
+
+export default function MyApp({ Component, pageProps }: AppProps) {
+	return (
+		<>
+			<Component {...pageProps} />
+			<Analytics />
+		</>
+	);
 }
